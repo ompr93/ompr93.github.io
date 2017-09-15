a1dc4180c494e10e7c9b53a4c153fab9355ba5c0
Om Prakash <ompr93@gmail.com>
2017-09-15 16:13:43 +0530

    Adding skillbar Jquery

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+
+$(document).ready(function() {
+  $('.skill-bar').addClass('skill-bar-full');
+});
